b89d3cd79490997db3d22f221c1f78327fdf0044
emilio <emilio@1633e602-76eb-4acd-8a1c-850ebfcfe024>
2009-07-04 13:28:35 +0000

    Se ha divido el modelo de la vista, y reutilizado el código entre la vista de roadmap y la de versión.
    git-svn-id: https://ociotec.com/svn/Software/Web/Redmine/plugins/advanced_roadmap/trunk@626 1633e602-76eb-4acd-8a1c-850ebfcfe024

--- init.rb
+++ init.rb
@@ -4,6 +4,7 @@ require "application"
 require "projects_controller"
 require "projects_controller_patch"
 require "issue_patch"
+require "version_patch"
 
 load_paths.each do |path|
   Dependencies.load_once_paths.delete(path)
@@ -13,7 +14,9 @@ RAILS_DEFAULT_LOGGER.info "Advanced roapmap plugin for RedMine"
 
 Redmine::Plugin.register :advanced_roapmap do
   name "Advanced roadmap plugin"
+  url ""
   author "Emilio Gonzalez"
+  author_url "http://ociotec.com"
   description "This is a plugin for Redmine that is used to show more information inside the Roadmap page"
-  version "0.0.1"
+  version "0.0.2"
 end
---
+++ lib/version_patch.rb
@@ -0,0 +1,81 @@
+module VersionPatch
+
+  def self.included(base)
+    base.extend(ClassMethods)
+    base.send(:include, InstanceMethods)
+    base.class_eval do
+      unloadable
+      alias_method_chain :completed_pourcent, :advanced_info
+      alias_method_chain :closed_pourcent, :advanced_info
+    end
+  end
+
+  module ClassMethods
+  end
+
+  module InstanceMethods
+
+    def completed_pourcent_with_advanced_info
+      calculate_advance_info unless @total_ratio
+      @total_ratio
+    end
+
+    def closed_pourcent_with_advanced_info
+      calculate_advance_info unless @total_finished_ratio
+      @total_finished_ratio
+    end
+
+    def rest_hours
+      calculate_advance_info unless @total_pending
+      @total_pending
+    end
+
+    def closed_spent_hours
+      if !@closed_spent_hours
+        @closed_spent_hours = 0.0
+        fixed_issues.each do |issue|
+          if issue.closed?
+            @closed_spent_hours += issue.spent_hours
+          end
+        end
+      end
+      @closed_spent_hours
+    end
+
+    def calculate_advance_info
+      total_estimated = 0.0
+      total_spent = 0.0
+      @total_pending = 0.0
+      @total_finished_ratio = 0.0
+      @total_ratio = 0.0
+      if fixed_issues.size > 0
+        fixed_issues.each do |issue|
+          if issue.estimated_hours && issue.done_ratio
+            ratio = issue.spent_hours / ((issue.estimated_hours * issue.done_ratio) / 100.0)
+          end
+          total_estimated += issue.estimated_hours ? issue.estimated_hours : 0.0
+          total_spent += issue.spent_hours ? issue.spent_hours : 0.0
+          @total_pending += issue.rest_hours ? issue.rest_hours : 0.0
+          if issue.spent_hours && issue.rest_hours
+            issue_time = (issue.spent_hours + issue.rest_hours) * issue.done_ratio
+            if issue.closed?
+              @total_finished_ratio += issue_time
+            end
+            @total_ratio += issue_time
+          end
+        end
+        if total_spent + @total_pending > 0.0
+          @total_finished_ratio /= (total_spent + @total_pending)
+          @total_ratio /= (total_spent + @total_pending)
+        else
+          @total_finished_ratio = 0.0        
+          @total_ratio = 0.0        
+        end
+      end
+    end
+
+  end
+
+end
+
+Version.send(:include, VersionPatch)
