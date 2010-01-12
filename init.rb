426e693d9f82880eb8f89d5616f8dc5d294c774a
emilio <emilio@1633e602-76eb-4acd-8a1c-850ebfcfe024>
2010-01-12 14:37:34 +0000

    #237 * Addapt to Redmine 0.9.0.
    git-svn-id: https://ociotec.com/svn/Software/Web/Redmine/plugins/advanced_roadmap/trunk@773 1633e602-76eb-4acd-8a1c-850ebfcfe024

--- init.rb
+++ init.rb
@@ -1,15 +1,12 @@
 require "redmine"
 require "gravatar"
-require "application"
+require "application_controller"
+require File.dirname(__FILE__) + "/../awesome_nested_set/rails/init"
 require "project_patch"
 require "projects_controller_patch"
 require "projects_helper_patch"
 require "issue_patch"
 require "version_patch"
-
-load_paths.each do |path|
-  Dependencies.load_once_paths.delete(path)
-end
 
 RAILS_DEFAULT_LOGGER.info "Advanced roapmap plugin for RedMine"
 
@@ -21,4 +18,5 @@ Redmine::Plugin.register :advanced_roapmap do
   description "This is a plugin for Redmine that is used to show more information inside the Roadmap page"
   version "0.0.6"
   permission :manage_milestones, {:milestones => [:add, :edit, :destroy]}
+  requires_redmine :version_or_higher => "0.9.0"
 end
